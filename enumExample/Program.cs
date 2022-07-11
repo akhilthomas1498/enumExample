70af7651d498492f23e9c8463e81a348cfafc4db
Akhil P Thomas <akhilthomas1498@gmail.com>
2022-07-11 11:13:17 +0530

    Add project files.

---
+++ enumExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExample
+{
+    enum Month
+    {
+        Jan,
+        Feb,
+        Mar,
+        Apr,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Month m = Month.Apr;
+            Console.WriteLine(m);
+            Console.ReadLine();
+        }
+    }
+}
